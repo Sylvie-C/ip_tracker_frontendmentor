817b98740fe3def1cb90f58cb88b218459814988
CSylvie <sylvie.castet@gmail.com>
2025-08-01 18:20:41 +0200

    style header with background images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 
+
 export default function Header() { 
 
   return (
-    <header className="relative z-0 flex flex-col items-center bg-gradient-to-r from-indigo-600 to-blue-400 h-64">
+    <header className="relative z-0 flex flex-col items-center h-64 bg-[url('/pattern-bg-mobile.png')] md:bg-[url('/pattern-bg-desktop.png')] bg-cover bg-center w-full">
       <h1 className="text-white text-xl md:text-3xl font-medium my-6">IP Address Tracker</h1>
     </header>
   )
