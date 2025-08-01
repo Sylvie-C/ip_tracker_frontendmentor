7788632ca42d6b35a73f963d75fb5d160208a5f1
CSylvie <sylvie.castet@gmail.com>
2025-08-01 18:10:00 +0200

    style data board

--- src/components/DataBoard.jsx
+++ src/components/DataBoard.jsx
@@ -1,26 +1,26 @@
 export default function DataBoard () {
 
   return(
-    <div className="p-6 w-full max-w-5xl h-fit min-h-40 rounded-xl bg-white shadow-lg text-center md:text-left flex flex-col md:flex-row justify-between items-center md:items-start gap-y-4 md:gap-y-0">
+    <div className="p-6 w-full max-w-5xl rounded-xl bg-white shadow-lg text-center md:text-left flex flex-col md:grid md:grid-cols-4 justify-between items-center md:items-start gap-y-4 md:gap-y-0">
 
-      <div className="flex flex-col gap-y-2">
-        <label className="text-gray-500 text-sm">IP ADDRESS</label>
-        <p className="text-black text-lg font-semibold">192.168.1.1</p>
+      <div className="flex flex-col gap-y-0.5 md:gap-y-2 h-full mx-4 border-r-neutral-200 md:border-r-2">
+        <label className="text-gray-400 text-xs font-bold">IP ADDRESS</label>
+        <p className="text-gray-800 text-base md:text-lg font-medium">192.168.1.1</p>
       </div>
 
-      <div className="flex flex-col gap-y-2">
-        <label className="text-gray-500 text-sm">LOCATION</label>
-        <p className="text-black text-lg font-semibold">Brooklyn, New York</p>
+      <div className="flex flex-col gap-y-0.5 md:gap-y-2 h-full mx-4 border-r-neutral-200 md:border-r-2">
+        <label className="text-gray-400 text-xs font-bold">LOCATION</label>
+        <p className="text-gray-800 text-base md:text-lg font-medium">Brooklyn, New York</p>
       </div>
 
-      <div className="flex flex-col gap-y-2">
-        <label className="text-gray-500 text-sm">TIME ZONE</label>
-        <p className="text-black text-lg font-semibold">UTC-5:00</p>
+      <div className="flex flex-col gap-y-0.5 md:gap-y-2 h-full mx-4 border-r-neutral-200 md:border-r-2">
+        <label className="text-gray-400 text-xs font-bold">TIME ZONE</label>
+        <p className="text-gray-800 text-base md:text-lg font-medium">UTC-5:00</p>
       </div>
 
-      <div className="flex flex-col gap-y-2">
-        <label className="text-gray-500 text-sm">ISP</label>
-        <p className="text-black text-lg font-semibold">Space X , Starlink</p>
+      <div className="flex flex-col gap-y-0.5 md:gap-y-2 h-full">
+        <label className="text-gray-400 text-xs font-bold">ISP</label>
+        <p className="text-gray-800 text-base md:text-lg font-medium">Space X , Starlink</p>
       </div>
 
     </div>
